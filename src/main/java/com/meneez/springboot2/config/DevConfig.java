b26c4a545a8218d47a894f0690298e3b1e023bff
Marcus Menezes <mvgmenezes@gmail.com>
2018-11-23 03:06:49 -0200

    Criando profile de Desenvolvimento

---
+++ src/main/java/com/meneez/springboot2/config/DevConfig.java
@@ -0,0 +1,41 @@
+package com.meneez.springboot2.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.meneez.springboot2.services.DBService;
+
+//classe com a configuracao somente para o profile de teste, application-dev.properties
+//@Profile("dev") - indico que os beans dessa classe so serao ativos quando o profile developer for usado 
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	//recupera o valor da chave spring.jpa.hibernate.ddl-auto=create dentro do application-dev.properties 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciateDatabase() throws ParseException {
+		
+		//verifico se a chave spring.jpa.hibernate.ddl-auto=create dentro do application-dev.properties 
+		//esta marcada como create para executar a instancia inicial
+		if (!"create".equals(strategy)) {
+			return false;
+		}else {
+			dbService.instantiateTestDatabase();
+		}
+		
+		
+		
+		return true;
+	}
+}
