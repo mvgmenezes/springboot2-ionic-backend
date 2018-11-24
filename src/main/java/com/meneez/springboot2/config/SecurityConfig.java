d2574c1d6ec05f79cbb5c7775b99bdce0438b100
Marcus Menezes <mvgmenezes@gmail.com>
2018-11-24 04:17:14 -0200

    Implementando a atutenticacao e implementacao do token JWT

--- src/main/java/com/meneez/springboot2/config/SecurityConfig.java
+++ src/main/java/com/meneez/springboot2/config/SecurityConfig.java
@@ -8,13 +8,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.meneez.springboot2.security.JWTAuthenticationFilter;
+import com.meneez.springboot2.security.JWTUtil;
 
 
 //implementando o JWT 
@@ -27,6 +33,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private Environment env;
 	
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JWTUtil jWTUtil;
 	
 	//acesso publico
 	private static final String[] PUBLIC_MATCHERS = {
@@ -57,8 +69,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
+		
+		//usando o filter
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jWTUtil));
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
+	
+	//usado no spring security para configurar
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
 	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
---
+++ src/main/java/com/meneez/springboot2/services/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.meneez.springboot2.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.meneez.springboot2.domain.Cliente;
+import com.meneez.springboot2.repositories.ClienteRepository;
+import com.meneez.springboot2.security.UserSpringSecurity;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		Cliente cli = clienteRepository.findByEmail(email);
+		
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		
+		return new UserSpringSecurity(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
